0e0b87e971d1e857cf56e781e89c5bfe423243a3
AJ1732 <ejemeniboi@gmail.com>
2024-04-22 10:47:09 +0100

    feat: create todo input

--- client/src/anatomy/Layout/Main/Main.jsx
+++ client/src/anatomy/Layout/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import TodoInput from '../../../components/Input/TodoInput'
 
 const Main = () => {
  
   return (
-    <main className='min-h-[calc(100dvh-170px)] mb-5 '>
-      Main
+    <main className='min-h-[calc(100dvh-140px)] font-poppins py-5 |'>
+      <section>
+        <h2 className='font-lora'>PERN Todos</h2>
+        <TodoInput /> 
+      </section>
     </main>
   )
 }
--- client/src/components/Input/TodoInput.jsx
+++ client/src/components/Input/TodoInput.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoInput = () => {
+  const [ description, setDescription ] = useState('')
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const todoBody = { description }
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(todoBody)
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+
+    console.log('Todo Submitted');
+  }
+
   return (
-    <div>TodoInput</div>
+    <form action='' onSubmit={handleSubmit} className='overflow-hidden | bg-shade py-7 px-5 drop-shadow-md'>
+      <fieldset className='space-y-6'>
+        {/* TITLE INPUT */}
+        <div className='grid gap-1'>
+          <label htmlFor="title">Todo Input</label>
+          <input 
+            id="title"
+            type="text" 
+            name="title" 
+            value={description}
+            placeholder='Enter Todo'
+            onChange={(e) => setDescription(e.target.value)}
+            className={`px-4 py-2 rounded`}
+           />
+        </div>
+
+        {/* SUBMIT BUTTON */}
+        <button className='bg-teal'>Submit</button>
+      </fieldset>
+
+      {/* {error && <div className='text-red-500 text-sm'>{error}</div>} */}
+    </form>
   )
 }
 
