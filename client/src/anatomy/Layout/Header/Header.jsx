5d91c968348f34ae6fb581d390b30cd8635e644f
AJ1732 <ejemeniboi@gmail.com>
2024-04-22 10:08:00 +0100

    style: style header

--- client/src/anatomy/Layout/Header/Header.jsx
+++ client/src/anatomy/Layout/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header >
-      <nav className='flex justify-between items-center | py-5 border-b border-shade'>
-        <h1 className='font-dm-serif font-semibold text-teal text-3xl self-start'>Header</h1>
+    <header className='full-width content-grid | | flex justify-center items-center'>
+      <nav className='flex justify-between items-center | py-5 border-b border-greyshade'>
+        <h1 className='font-dm-serif font-semibold text-teal text-3xl self-start'>Todo List</h1>
       </nav>
     </header>
   )
